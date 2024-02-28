59ddd3103911a576395d732b25f4b02272220ee5
Valéria Santiago <101909202+ValSantiago@users.noreply.github.com>
2024-02-27 23:29:15 -0300

    Add files via upload
    Exercícío 1

---
+++ EX01.js
@@ -0,0 +1,7 @@
+// Crie um array chamado frutas contendo três frutas diferentes. Em seguida, imprima no console a segunda fruta do array.
+
+const prompt = require('prompt-sync')();
+
+const frutas = ["banana", "manga", "caqui"];
+
+console.log(`A segunda fruta é: ${frutas[1]}`);
